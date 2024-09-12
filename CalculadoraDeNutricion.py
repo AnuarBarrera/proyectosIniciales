6f1435f63c964c1ba58b21b48f556cc289bc756c
Anuar Barrera MotoEdge <contacto.neia@gmail.com>
2024-09-11 19:08:44 -0600

    Please enter the commit message for your changes. Lines starting  Initial structure by algoritm

---
+++ CalculadoraDeNutricion.py
@@ -0,0 +1,26 @@
+#readmy CalculadoraDeNutricion is un algoritm what calculate the count protein, sugar and fats have the product.
+
+class CalculadoraDeNutricion ():
+  #variants
+  gramsExent = float
+  portion = float
+  sugar = float
+  protein = float
+  fats = float
+  result = float
+  
+  def inputInformation(self):
+  
+  def calculate(self):
+    
+  def showResult(self):
+  
+class menu():
+  
+  def showOptions(self):
+    
+  def inputOption(self):
+    
+
+    
+    
