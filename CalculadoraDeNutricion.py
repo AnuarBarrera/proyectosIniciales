bb4208b1fed06540be4cb908a9f74ad540ca4ae2
AnuarBarrera MotoAzulado <contacto.neia@gmail.com>
2024-09-20 21:03:28 -0600

    changes in the README.md and issue in the CalculadoraDeNutricion.py

--- CalculadoraDeNutricion.py
+++ CalculadoraDeNutricion.py
@@ -1,17 +1,17 @@
 #readmy CalculadoraDeNutricion is un algoritm what calculate the content protein, sugar and fats have the product.
 
-class CalculadoraDeNutricion ():
-  #variables
-  gramsExent = 0.0
-  portion = 0.0
-  sugar = 0.0
-  protein = 0.0
-  fats = 0.0
-  result = 0.0
-  
-  #Entrance Information
-  def inputInformation(self):
-    pass
+#SE DEBE CORREGIR LA HERENCIA ENTRE LAS 2 CLASES
+
+class CalculadoraDeNutricion():
+  def __init__(self):
+    #Entrance Information
+    def inputInformation(self):
+      return input("you write the count from grams the sample, portion, sugar, protein and fats in that order and separated with comma (ejem: 100,150,80, etc")
+   
+  #DataBase
+  def ChargeDataBase(self,variables,DictionaryVariables):
+    return dict(zip(DictionaryVariables.keys(),variables))
+    
   #Calculations
   def calculate(self):
     pass
@@ -20,21 +20,24 @@ class CalculadoraDeNutricion ():
   def showResult(self):
     pass
 
-#abstraction, User interface is diferent to the process operations
+#Class mmMenu show message the user
 
-class menu():
+class Menu(CalculadoraDeNutricion):
+  def __init__(self):
+    super().__init__()
   
   def messageWelcome(self):
-    print ("Welcome to Calculadora De Nutricion\nI will help to calculate the content\nwhit Proteins, Sugar and fat whit any product\nYou get into some data\nfor know the nutrimental value whit the product")
+    print ("Welcome to Calculadora De Nutricion\nI will help to calculate the content\nwhit Proteins, Sugar and fat from any product\nYou get into some data\nfor know the nutrimental value from the product")
     
-  def showOptions(self):
-    pass
-    
-  def inputOption(self):
-    pass
 
-callTheMessageWelcome = menu()
-callTheMessageWelcome.messageWelcome()
+
+start = Menu()
+start.messageWelcome()
+variables = start.inputInformation()
+DictionaryVariables = {"sample: ","portion: ","sugar: ","protein: ","sugar: "}
+data = star.ChargeDataBase(variables,DictionaryVariables)
+
+
     
 
     
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Welcome to my repositoriy "proyectosiniciales".
 
 I make it's repository for load my workhome's in the diferents courses and proyects.
 
-My carrer plan is be Data Engineer or Software Engineer. I show big advanceed in the Machine learnig and IA  what it make
+My carrer plan is be Data Engineer and Software Engineer. I show big advanceed in the Machine learnig and IA  what it make
 necessary learn and know Programming Lenguague.
 
 thank for readmme.
