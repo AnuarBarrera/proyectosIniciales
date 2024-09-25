40fe27db51951c0b3cab24c23732ee1a592a8c60
AnuarBarrera <contacto.neia@gmail.com>
2024-09-24 20:08:45 -0600

    I was fix the issue with the objects, create 3 instances for abstracted

--- CalculadoraDeNutricion.py
+++ CalculadoraDeNutricion.py
@@ -1,44 +1,54 @@
 #readmy CalculadoraDeNutricion is un algoritm what calculate the content protein, sugar and fats have the product.
 
-#SE DEBE CORREGIR LA HERENCIA ENTRE LAS 2 CLASES
-
-class CalculadoraDeNutricion():
-  def __init__(self):
-    #Entrance Information
-    def inputInformation(self):
-      return input("you write the count from grams the sample, portion, sugar, protein and fats in that order and separated with comma (ejem: 100,150,80, etc")
-   
-  #DataBase
-  def ChargeDataBase(self,variables,DictionaryVariables):
-    return dict(zip(DictionaryVariables.keys(),variables))
-    
-  #Calculations
-  def calculate(self):
-    pass
+#Class Usuario show interface the usuario
+class Usuario():
+  
+  def messageWelcome(self):
+    print ("Welcome to Calculadora De Nutricion\nI will help to calculate the content\nwhit Proteins, Sugar and fat from any product\nYou get into some data\nfor know the nutrimental value from the product")
+  
+  #Entrance Information
+  def inputInformation(self):
+    return input("you write the count from grams the sample, portion, sugar, protein and fats in that order and separated with comma (ejem: 100,150,80, etc")
     
   #Exit Information
   def showResult(self):
     pass
 
-#Class mmMenu show message the user
-
-class Menu(CalculadoraDeNutricion):
-  def __init__(self):
-    super().__init__()
-  
-  def messageWelcome(self):
-    print ("Welcome to Calculadora De Nutricion\nI will help to calculate the content\nwhit Proteins, Sugar and fat from any product\nYou get into some data\nfor know the nutrimental value from the product")
+#Class TransformData is one implementation between the entrence information and el process data in CalculadoraDeNutricion
+class TransformData():
+  def __init__(self,data,number):
+    self.data = data
+    self.number = number
     
+  def transform(self,data,number):
+    
+    value = 0
+    
+    for i in range(0,4):
+      if i == number:
+        value = data[i]
+        
+    return value
+    
+#class CalculadoraDeNutricion is the backend from application
+class CalculadoraDeNutricion():
+  def __init__(self,sample,portion,incognit):
+    self.sample = sample
+    self.portion = portion
+    self.incognit = incognit
+ 
+  #Calculations
+  def calculate(self,sample,portion,incognit):
+    return (portion * incognit) / sample
 
-
-start = Menu()
+start = Usuario()
 start.messageWelcome()
-variables = start.inputInformation()
-DictionaryVariables = {"sample: ","portion: ","sugar: ","protein: ","sugar: "}
-data = star.ChargeDataBase(variables,DictionaryVariables)
+data = start.inputInformation()
 
+sample = TransformData(data,0)
+portion = TransformData(data,1)
+sugar= TransformData(data,2)
+protein = TransformData(data,3)
+fat = TransformData(data,4)
 
-    
-
-    
     
